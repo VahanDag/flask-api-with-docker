c628cdbbcf7adba0dafaf36d4bbb3ee78ace6ecc
Vahan Dağ <vahandag@gmail.com>
2024-01-26 19:56:14 +0300

    second

---
+++ app/models.py
@@ -0,0 +1,13 @@
+from datetime import datetime
+
+from app import db
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(50), unique=True, nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return f"<User {self.name}>"
