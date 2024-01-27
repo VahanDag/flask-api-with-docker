efb8be25eb2ae75aba5c23326de858bf77bd06e8
Vahan Dağ <vahandag@gmail.com>
2024-01-28 01:23:15 +0300

    Son düzeltmeler yapıldı

--- app/__init__.py
+++ app/__init__.py
@@ -1,16 +1,13 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+from prometheus_flask_exporter import PrometheusMetrics
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////usr/src/app/data/users.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
 
 from app import routes
-
-with app.app_context():
-    db.create_all()
 
 if __name__ == '__main__':
     app.run(debug=True)
 
+metrics = PrometheusMetrics(app)
+
+metrics.start_http_server(9090)
--- app/routes.py
+++ app/routes.py
@@ -1,39 +1,10 @@
 from flask import jsonify, request
 
-from app import app, db
-from app.models import User
+from app import app
 
 
-@app.route('/users', methods=['GET'])
-def get_users():
-    users = User.query.all()
-    return jsonify([{'id': user.id, 'name': user.name, 'email': user.email} for user in users]), 200
-
-@app.route('/users/<int:user_id>', methods=['GET'])
-def get_user(user_id):
-    user = User.query.get_or_404(user_id)
-    return jsonify({'id': user.id, 'name': user.name, 'email': user.email}), 200
-
-@app.route('/users', methods=['POST'])
-def add_user():
+@app.route('/echo-name', methods=['POST'])
+def echo_name():
     data = request.json
-    new_user = User(name=data['name'], email=data['email'])
-    db.session.add(new_user)
-    db.session.commit()
-    return jsonify({'message': 'User created successfully.', 'id': new_user.id}), 201
-
-@app.route('/users/<int:user_id>', methods=['PUT'])
-def update_user(user_id):
-    user = User.query.get_or_404(user_id)
-    data = request.json
-    user.name = data['name']
-    user.email = data['email']
-    db.session.commit()
-    return jsonify({'message': 'User updated successfully.'}), 200
-
-@app.route('/users/<int:user_id>', methods=['DELETE'])
-def delete_user(user_id):
-    user = User.query.get_or_404(user_id)
-    db.session.delete(user)
-    db.session.commit()
-    return jsonify({'message': 'User deleted successfully.'}), 200
+    name = data.get('name', 'No Name Provided')
+    return jsonify({'message': f'Hello, {name}!'}), 200
